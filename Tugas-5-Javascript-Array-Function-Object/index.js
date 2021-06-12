c731059b9b0bcc38d0f4d5a3977ccd0528c5b11b
afirsta <afirsta15@gmail.com>
2021-06-12 07:45:27 +0700

    num 2
    gyfehsdrft

--- Tugas-5-Javascript-Array-Function-Object/index.js
+++ Tugas-5-Javascript-Array-Function-Object/index.js
@@ -1,6 +1,4 @@
 //soal 1
-
-
 var daftarHewan = 
 		['2. Komodo', 
 		'5. Buaya', 
@@ -18,3 +16,18 @@ for (var i = 1; i < daftarHewan.length; i++)
         }
 
 console.log(daftarHewan);
+
+//soal 2
+var data = perkenalan
+function perkenalan(nama, age, address, hobby)
+	{
+	console.log("Nama saya " + name_param + ", umur saya " + age_param + ", alamat saya di " + address_param + ", dan saya punya hobby yaitu " + hobby_param);
+	}
+
+var name : "John"
+var age : "30" 
+var address : "Jalan Pelesiran"
+var hobby : "Gaming" 
+
+var perkenalan = intorduce(name, age,adress, hobby)
+console.log(perkenalan) 
