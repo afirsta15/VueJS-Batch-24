c80abc5e386c046d7c266ebd9779e602b36c920d
afirsta <afirsta15@gmail.com>
2021-06-16 00:22:33 +0700

    add file index2.js
    add file index2.js

---
+++ Tugas-7-Asynchronous/index2.js
@@ -0,0 +1,13 @@
+var readBooksPromise = require('./promise.js')
+ 
+var books = [
+    {name: 'LOTR', timeSpent: 3000}, 
+    {name: 'Fidas', timeSpent: 2000}, 
+    {name: 'Kalkulus', timeSpent: 4000}
+]
+ 
+// Lanjutkan code untuk menjalankan function readBooksPromise 
+return new readBooksPromise( function (resolve, reject){
+     setTimeout (function(){
+        setTimeout(() => resolve, 1000);});
+      
